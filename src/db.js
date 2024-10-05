04b83fdfa66cc0dc6e2073cd8483f5a033216390
Virginia <virginia.vilchez.t@gmail.com>
2024-10-05 14:17:03 +0200

    db & API

--- src/db.js
+++ src/db.js
@@ -1,26 +1,24 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
+async function getConnection() {
+	try {
+		const conn = await mysql.createConnection({
+			host: "sql.freedb.tech",
+			port: 3306,
+			user: "freedb_PatriXcX",
+			password: "#bH7UZrC%4T32fQ",
+			database: "freedb_proyecto_team_2_4",
+		});
 
-async function getConnection () {
-  try {
-    const conn = await mysql.createConnection({
-      host: process.env.DB_host,
-      port: process.env.DB_port,
-      user: process.env.DB_user,
-      password: process.env.DB_password,
-      database: process.env.DB_database,
-    });
+		await conn.connect();
 
-    await conn.connect();
+		return conn;
+	} catch (error) {
+		console.log(error);
 
-    return conn;
-  }
-  catch(error) {
-    console.log(error);
-    
-    return null;
-  }
+		return null;
+	}
 }
 
 module.exports = getConnection;
--- src/server.js
+++ src/server.js
@@ -1,21 +1,19 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 function getServer() {
-  const server = express();
+	const server = express();
 
-  server.use(cors());
-  server.use(express.json());
-  
-  const port = 4000;
-  
-  server.listen( port, () => {
-    console.log(`Servidor iniciado escuchando en http://localhost:${port}`);
-  });
-  server.use(express.static('./public'));
-  return server
+	server.use(cors());
+	server.use(express.json({ limit: "10mb" }));
+
+	const port = 4000;
+
+	server.listen(port, () => {
+		console.log(`Servidor iniciado escuchando en http://localhost:${port}`);
+	});
+	server.use(express.static("./public"));
+	return server;
 }
-
-
 
 module.exports = getServer;
