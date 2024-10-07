d0688901e902c7c70786f9e0451ca6e8d8421bbd
evasanzgue <evasanzgue@gmail.com>
2024-10-07 20:31:14 +0200

    finish the endpoint

--- src/index.js
+++ src/index.js
@@ -4,53 +4,74 @@ const cors = require("cors");
 
 const server = getServer();
 
-server.use(cors()); // Habilitar CORS para todas las rutas
-
-
-// obtener todo el listado del proyecto de la base de datos
-server.get("/api/projectCard", async (req, res) => {
-  // Obtener conn con MySQL
+server.use(cors());
+server.get('/api/projectCard', async (req, res) => {
   const conn = await getConnection();
   if (!conn) {
     res.status(500).json({ success: false, error: "Error con la conexion." });
     return;
   }
-  // Lanzar una query
-  const [results] = await conn.query(
-    "SELECT * FROM autora, proyectos  WHERE idautora = autora_idautora;"
-  );
-  // Devolvemos los resultados como JSON
-  console.log("Resultados obtenidos de la base de datos:", results);
-  res.json(results);
-  // Cerrar la conn
+  const [results] = await conn.query("SELECT * FROM autora, proyectos  WHERE idautora = autora_idautora;");
+  res.json({ success: true, data: results });
   await conn.close();
 });
 
-server.post("/api/projectCard", async (req, res) => {
+
+server.post('/api/projectCard', async(req, res) => {
+
   const conn = await getConnection();
   if (!conn) {
     res.status(500).json({ success: false, error: "Error con la conexion." });
     return;
   }
+  try {
+    await conn.beginTransaction();
+    const [existeAutora]  = await conn.query("SELECT * FROM autora WHERE nombre_autora like ?", [req.body.autor]);
+
+    let autoraId
+    if (existeAutora.length == 0) {
+      const [autoraResults] = await conn.query(
+        'INSERT INTO `autora` (`nombre_autora`, `trabajo`, `foto_autora`) VALUES (?, ?, ?);',
+        [ req.body.autor, req.body.job, req.body.photo ]
+      );
+      autoraId = autoraResults.insertId
+    } else {
+      autoraId = existeAutora[0].idautora
+    }
+
+    const [proyectResults] = await conn.query('INSERT INTO `proyectos` (`autora_idautora`,`nombre_proyecto`, `slogan`, `repositorio`, `demo`, `tecnologias`, `descripcion`, `foto_proyecto` ) VALUES (?, ?, ?, ?, ?, ?, ?, ?);',
+    [ autoraId, req.body.name, req.body.slogan, req.body.repo, req.body.demo, req.body.technologies, req.body.desc, req.body.image ]);
+
+    await conn.commit();
+    res.json({ success: true, id: proyectResults.insertId });
+  } catch (err) {
+    await conn.rollback();
+    res.status(400).json({ success: false });
+  }
+  await conn.close();
+});
+
+server.get('/api/projectCard/:id', async (req, res) => {
+  const conn = await getConnection();
+  if (!conn) {
+    res.status(500).json({ success: false, error: "Error con la conexion." });
+    return;
+  }
+  
+  const projectId = req.params.id;
+
   const [results] = await conn.query(
-    "INSERT INTO `autora` (`nombre_autora`, `trabajo`, `foto_autora`) VALUES (?, ?, ?);",
-    [req.body.autor, req.body.job, req.body.photo]
+    "SELECT * FROM autora, proyectos WHERE idautora = autora_idautora AND idproyectos = ?",
+    [projectId]
   );
 
-  const [result] = await conn.query(
-    "INSERT INTO `proyectos` (`autora_idautora`,`nombre_proyecto`, `slogan`, `repositorio`, `demo`, `tecnologias`, `descripcion`, `foto_proyecto` ) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
-    [
-      results.insertId,
-      req.body.name,
-      req.body.slogan,
-      req.body.repo,
-      req.body.demo,
-      req.body.technologies,
-      req.body.desc,
-      req.body.image,
-    ]
-  );
-
-  console.log(result);
-  res.status(404).json({ message: "proyecto no integrado correctamente" });
+  if (results.length > 0) {
+    res.json({
+      success: true,
+      data: results[0]
+    });
+  } else {
+    res.status(404).json({ success: false});
+  }
+  await conn.close();
 });
